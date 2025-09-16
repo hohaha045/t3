88dba36adfc3b63d9db3e54b85105845de743e32
Abdullah-145 <114139402+Abdullah-145@users.noreply.github.com>
2025-09-17 00:14:45 +0500

    Update config.h

--- keyboards/bastardkb/charybdis/3x5/keymaps/neville/config.h
+++ keyboards/bastardkb/charybdis/3x5/keymaps/neville/config.h
@@ -39,12 +39,13 @@
 /* Charybdis-specific features. */
 
 #ifdef POINTING_DEVICE_ENABLE
-#define POINTING_DEVICE_SCROLL_ENABLE
-#define POINTING_DEVICE_INVERT_V
-// Automatically enable the pointer layer when moving the trackball.  See also:
-// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS`
-// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD`
-// #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+    #define POINTING_DEVICE_SCROLL_ENABLE
+    #define POINTING_DEVICE_INVERT_V
+
+    // Automatically enable the pointer layer when moving the trackball.
+    #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+    #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS 200
+    #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD 8
 #endif // POINTING_DEVICE_ENABLE
 
 #define COMBO_COUNT 3 // Change this number.
