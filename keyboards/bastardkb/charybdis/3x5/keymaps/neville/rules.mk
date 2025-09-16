406f523b1dbd90a4a1fee6f3a651940158562584
Abdullah-145 <114139402+Abdullah-145@users.noreply.github.com>
2025-09-17 00:24:01 +0500

    Update rules.mk

--- keyboards/bastardkb/charybdis/3x5/keymaps/neville/rules.mk
+++ keyboards/bastardkb/charybdis/3x5/keymaps/neville/rules.mk
@@ -1,11 +1,20 @@
+# Enable VIA for remapping
 VIA_ENABLE = yes
+
+# Enable mouse keycodes
 MOUSEKEY_ENABLE = yes
+
+# Enable combo feature
 COMBO_ENABLE = yes
+
+# Disable tap dance
 TAP_DANCE_ENABLE = no
+
+# Allow per-key tapping term
 DYNAMIC_TAPPING_TERM_ENABLE = yes
 
-
-
-//here i have to this change to define heeader file 
+# Enable debugging console (for printf/logging)
 CONSOLE_ENABLE = yes
+
+# Enable trackball / trackpad support (PMW3360 + Cirque)
 POINTING_DEVICE_ENABLE = yes
