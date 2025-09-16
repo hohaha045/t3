97072797aec3f42ebaef26dfc7789e6141290009
Abdullah-145 <114139402+Abdullah-145@users.noreply.github.com>
2025-09-16 13:25:49 +0500

    Update rules.mk

--- keyboards/bastardkb/charybdis/3x5/keymaps/neville/rules.mk
+++ keyboards/bastardkb/charybdis/3x5/keymaps/neville/rules.mk
@@ -3,3 +3,9 @@ MOUSEKEY_ENABLE = yes
 COMBO_ENABLE = yes
 TAP_DANCE_ENABLE = no
 DYNAMIC_TAPPING_TERM_ENABLE = yes
+
+
+
+//here i have to this change to define heeader file 
+CONSOLE_ENABLE = yes
+POINTING_DEVICE_ENABLE = yes
